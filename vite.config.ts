9fe4fff97988361f920cc2b19e02edc89aa2769b
YOONJEONG KIM <yoon@YOONJEONGui-MacBookAir.local>
2025-09-16 18:47:35 +0900

    fix: remove dangling comma in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      plugins: [react()],
+      base: './', 
       // define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
